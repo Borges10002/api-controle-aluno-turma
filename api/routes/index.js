b039e821494f4893ac10e928b581cfa613249a55
borges10002 <borges10002@gmail.com>
2022-09-07 07:37:37 -0400

    ORM com NodeJS avançando nas funcionalidades do Sequelize ORM com NodeJS avançando nas funcionalidades do Sequelize

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,11 +5,10 @@ const niveis = require('./niveisRoute')
 const turmas = require('./turmasRoute')
 
 module.exports = app => {
- app.use(
-   bodyParser.json(),
-   bodyParser.urlencoded({ extended: false }),
-   pessoas,
-   niveis,
-   turmas
-   )
- }
+  app.use(
+    bodyParser.json(),
+    pessoas,
+    niveis,
+    turmas
+  )
+}
--- api/routes/niveisRoute.js
+++ api/routes/niveisRoute.js
@@ -3,9 +3,10 @@ const NivelController = require('../controllers/NivelController')
  
 const router = Router()
 router
- .get('/niveis', NivelController.pegaTodosOsNiveis)
- .get('/niveis/:id', NivelController.pegaUmNivel)
- .post('/niveis', NivelController.criaNivel)
- .put('/niveis/:id', NivelController.atualizaNivel)
- .delete('/niveis/:id', NivelController.apagaNivel)
+  .get('/niveis', NivelController.pegaTodosOsNiveis)  
+  .get('/niveis/:id', NivelController.pegaNivel)  
+  .post('/niveis', NivelController.criaNivel)  
+  .post('/niveis/:id/restaura', NivelController.restauraNivel)  
+  .put('/niveis/:id', NivelController.atualizaNivel)  
+  .delete('/niveis/:id', NivelController.apagaNivel)  
 module.exports = router
